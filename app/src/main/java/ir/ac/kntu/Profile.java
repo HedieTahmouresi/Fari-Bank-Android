92bc912c1069105130ed1af1095e5823ffbeb063
@0025755900 <hedieh.tahmouresi@email.kntu.ac.ir>
2024-07-09 20:03:06 +0330

    created the profile page

--- app/src/main/java/ir/ac/kntu/Profile.java
+++ app/src/main/java/ir/ac/kntu/Profile.java
@@ -1,6 +1,9 @@
 package ir.ac.kntu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Profile extends AppCompatActivity {
+    private ImageView contact;
+    private ImageView support;
+    private ImageView settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,52 @@ public class Profile extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        String phoneNumber = getIntent().getStringExtra("Phone Number");
+        SimpleUser currentUser = MainActivity.getCurrentUser(phoneNumber);
+        onClickContact(currentUser);
+        onClickSettings(currentUser);
+        onClickSupport(currentUser);
+    }
+
+    public void onClickContact(SimpleUser currentUser){
+        contact = (ImageView) findViewById(R.id.arrowContact);
+        contact.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(Profile.this, ContactPage.class);
+                        intent.putExtra("Phone Number",currentUser.getSimCard().getPhoneNumber());
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+
+    public void onClickSupport(SimpleUser currentUser){
+        support = (ImageView) findViewById(R.id.arrowSupport);
+        support.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(Profile.this, Support.class);
+                        intent.putExtra("Phone Number",currentUser.getSimCard().getPhoneNumber());
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+
+    public void onClickSettings(SimpleUser currentUser){
+        settings = (ImageView) findViewById(R.id.arrowSettings);
+        settings.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(Profile.this, Settings.class);
+                        intent.putExtra("Phone Number",currentUser.getSimCard().getPhoneNumber());
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
